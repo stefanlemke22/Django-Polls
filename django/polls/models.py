6e28c380f99c2f60bcc47ec8470ad6f40b9aea46
Stefan Lemke <stefanlemke22@gmail.com>
2017-07-20 12:05:04 -0400

    wrote the polls app to add questions and choices

---
+++ django/polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{question}: {choice}'.format(
+            question=self.question,
+            choice=self.text)
+# Create your models here.
